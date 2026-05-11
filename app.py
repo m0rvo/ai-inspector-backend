24c2ca7f78bed933a7ad7a4b5c44e4da8c88173d
m0rvo <vovam387@gmail.com>
2026-05-11 12:47:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,4 @@ app = Flask(__name__)
 @app.route('/analyze', methods=['POST'])
 def analyze():
     file = request.files['image']
-
-    # пока простая логика (позже подключим ML)
-    result = "GOOD"
-
-    return jsonify({"result": result})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    return jsonify({"result": "GOOD"})
